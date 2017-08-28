70ddad445d4c840bc704cb43f3ec056998192d8a
fkenghagho <fkenghag@uni-bremen.de>
2017-08-28 18:06:55 +0200

    :unicorn: structure of the dataset
    structure of the dataset

--- tools/generateDataset.py
+++ tools/generateDataset.py
@@ -2,9 +2,22 @@ import numpy as np
 import os 
 import sys
 import time
-
+from unrealcv import client
 
 class Dataset(object):
     def __init__(self,folder,nberOfImages):
         self.folder=folder
         self.nberOfImages=nberOfImages
+        self.client.connect()
+ 
+
+
+    def scan():
+        try:
+            p=self.client.request('vget /camera/0/lit')
+            a=p.split('/').pop()
+            p=self.client.request('vget /camera/0/object_mask '+a)
+            print p
+        except Exception,e:
+            print 'Image not saved: error occured, '+str(e)
+    
